b9ebf23249554d1e4b4777d0864024faba596dd5
xander <73213620+xandner@users.noreply.github.com>
2022-08-21 16:29:06 +0430

    Update main.ts
    test commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
+  console.log("×××")
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
