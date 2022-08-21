f7da8b42cd8b39280330eac7fe807432d2d0fd3c
xander <karimi.xander@gmail.com>
2022-08-21 10:11:33 -0700

    unit test

---
+++ src/users/auth.revice.spec.ts
@@ -0,0 +1,19 @@
+import { Test } from "@nestjs/testing"
+import { AuthService } from "./auth.service"
+import { UsersService } from "./users.service"
+
+it("can create instance of auth service",async ()=>{
+    const fakeUserService={
+        find:()=>Promise.resolve([]),
+        create:(email:string,password:string)=>Promise.resolve({id:1,email,password})
+    }
+    const module=await Test.createTestingModule({
+        providers:[AuthService,{
+            provide:UsersService,
+            useValue:fakeUserService
+        }]
+    }).compile()
+
+    const service=module.get(AuthService)
+    expect(service).toBeDefined();
+})
